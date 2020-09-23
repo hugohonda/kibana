13ee2e4cd4400e9368697d7e4eb0ef1aa115ea16
Hugo Honda <hugohondaf@gmail.com>
2020-09-23 19:34:10 -0300

    remove parentethesis from pie chart when its previous label is empty

--- src/plugins/vis_type_vislib/public/vislib/visualizations/pie_chart.js
+++ src/plugins/vis_type_vislib/public/vislib/visualizations/pie_chart.js
@@ -244,7 +244,13 @@ export class PieChart extends Chart {
             // modified by HHonda
             // const value = numeral(d.value / 100).format('0.[00]%');
             const value = numeral(d.value / 100).format('0,[00]%');
-            return `${d.name} (${value})`;
+            const output = `|${d.name}| (${value})`
+            if (d.name.match(/^\s+$/)) {
+              return value;
+            }
+            // modified by HHonda
+            // return `${d.name} (${value})`;
+            return output;
           }
           return d.name;
         })
